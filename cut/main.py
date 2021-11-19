8d070e3b837b910f1e62e7bf9d16db45c0032830
xuyipeng <1278338535@qq.com>
2021-11-19 09:17:48 +0800

    视频转换图片完成 增加了一些备注

--- cut/main.py
+++ cut/main.py
@@ -1,23 +1,23 @@
 import os
 import cv2
 
-cut_frame = 14  # 多少帧截一次，自己设置就行
+cut_frame = 50  # 多少帧截一次，自己设置就行
 save_path = "D:\workspace\FPS-\cut\p" #保存图片的文件夹
 
 for root, dirs, files in os.walk(r"D:\workspace\FPS-\cut\v"):  # 这里就填文件夹目录就可以了
     for file in files:
         # 获取文件路径
         if ('.mp4' in file):
-            path = os.path.join(root, file)
-            video = cv2.VideoCapture(path)
-            video_fps = int(video.get(cv2.CAP_PROP_FPS))
+            path = os.path.join(root, file) #把路径和文件名合成一个路径
+            video = cv2.VideoCapture(path) #这是读取视频文件
+            video_fps = int(video.get(cv2.CAP_PROP_FPS)) #获取视频的帧率
             print(video_fps)
             current_frame = 0
             while (True):
-                ret, image = video.read()
+                ret, image = video.read() #按帧读取视频 ret,image是两个返回值 ret是布尔值,image是图像是一个三维数组 ret读到正确的帧就是true 如果读到文件的结尾就是false
                 current_frame = current_frame + 1
                 if ret is False:
-                    video.release()
+                    video.release() #释放
                     break
                 if current_frame % cut_frame == 0:
                     # cv2.imwrite(save_path + '/' + file[:-4] + str(current_frame) + '.jpg',
